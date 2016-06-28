7155aa0835bffec789bd2f7302b96cd3c12129ac
matthewspindel <rebeldevelopers5@gmail.com>
2016-06-28 01:16:12 -0700

    second commit

---
+++ app/src/main/java/com/vivir/helloworld/Test.java
@@ -0,0 +1,12 @@
+package com.vivir.helloworld;
+
+/**
+ * Created by matthewspindel on 6/28/16.
+ */
+public class Test {
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
